e65f57c2895c8f9a7dfa4e7db07f31d887211cee
Arman Ali <armnsrn173200@gmail.com>
2023-05-12 03:24:43 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,38 @@
+const hamburger = document.querySelector('.hamburger');
+const mobileNav = document.querySelector('.mobile-nav');
+const overlay = document.querySelector('.overlay');
+const contactForm = document.querySelector('.contact-form');
+
+hamburger.addEventListener('click', () => {
+  mobileNav.classList.toggle('active');
+  hamburger.classList.toggle('active');
+  overlay.classList.toggle('active');
+});
+
+function closeMobileNav(){
+  mobileNav.classList.remove('active');
+  hamburger.classList.remove('active');
+  overlay.classList.remove('active');
+}
+
+overlay.addEventListener("click", closeMobileNav);
+
+contactForm.addEventListener('submit', (event) => {
+  // event.preventDefault();
+  alert("your response has been successfully submitted")}
+
+  );
+
+
+var typeData = new Typed(".role", {
+    strings: [
+      "Web Developer",
+      "Frontend Developer",
+      "Full Stack Developer",
+      "Coder",
+    ],
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 80,
+    backDelay: 1000,
+  });
